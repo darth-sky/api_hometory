156baaf3af6828e2b8a11006b9fe68033225e801
darth-sky <darthskywalker245@gmail.com>
2024-06-12 14:41:52 +0800

    commit kedua

--- api/barang_dlm_container/endpoints.py
+++ api/barang_dlm_container/endpoints.py
@@ -4,66 +4,66 @@ from flask import Blueprint, jsonify, request
 from helper.db_helper import get_connection
 from helper.form_validation import get_form_data
 
-barang_dlm_ruangan_endpoints = Blueprint('barang_dlm_ruangan', __name__)
+barang_dlm_container_endpoints = Blueprint('barang_dlm_container', __name__)
 UPLOAD_FOLDER = "img"
 
 
-@barang_dlm_ruangan_endpoints.route('/read', methods=['GET'])
+@barang_dlm_container_endpoints.route('/read', methods=['GET'])
 def read():
     """Routes for module get list books"""
     connection = get_connection()
     cursor = connection.cursor(dictionary=True)
-    select_query = "SELECT * FROM barang_dlm_ruangan"
+    select_query = "SELECT * FROM barang_dlm_container"
     cursor.execute(select_query)
     results = cursor.fetchall()
     cursor.close()  # Close the cursor after query execution
     connection.close()
     return jsonify({"message": "OK", "datas": results}), 200
 
-@barang_dlm_ruangan_endpoints.route('/read/<id_ruangan>', methods=['GET'])
-def readid(id_ruangan):
+@barang_dlm_container_endpoints.route('/read/<id_container>', methods=['GET'])
+def readid(id_container):
     """Routes for module get list books"""
     connection = get_connection()
     cursor = connection.cursor(dictionary=True)
-    select_query = "SELECT * FROM barang_dlm_ruangan WHERE id_ruangan=%s"
-    cursor.execute(select_query, (id_ruangan,))
+    select_query = "SELECT * FROM barang_dlm_container WHERE id_container=%s"
+    cursor.execute(select_query, (id_container,))
     results = cursor.fetchall()
     cursor.close()  # Close the cursor after query execution
     connection.close()
     return jsonify({"message": "OK", "datas": results}), 200
 
 
-@barang_dlm_ruangan_endpoints.route('/create', methods=['POST'])
+@barang_dlm_container_endpoints.route('/create', methods=['POST'])
 def create():
     """Routes for module create a book"""
-    id_ruangan = request.form['id_ruangan']
-    nama_barang_dlm_ruangan = request.form['nama_barang_dlm_ruangan']
-    desc_barang_dlm_ruangan = request.form['desc_barang_dlm_ruangan']
-    qnty_barang_dlm_ruangan = request.form['qnty_barang_dlm_ruangan']
+    id_container = request.form['id_container']
+    nama_barang_dlm_container = request.form['nama_barang_dlm_container']
+    desc_barang_dlm_container = request.form['desc_barang_dlm_container']
+    qnty_barang_dlm_container = request.form['qnty_barang_dlm_container']
 
-    uploaded_file = request.files['gambar_barang_dlm_ruangan']
+    uploaded_file = request.files['gambar_barang_dlm_container']
     if uploaded_file.filename != '':
         file_path = os.path.join(UPLOAD_FOLDER, uploaded_file.filename)
         uploaded_file.save(file_path)
 
         connection = get_connection()
         cursor = connection.cursor()
-        insert_query = "INSERT INTO barang_dlm_ruangan (id_ruangan, nama_barang_dlm_ruangan, desc_barang_dlm_ruangan, qnty_barang_dlm_ruangan, gambar_barang_dlm_ruangan) VALUES (%s, %s, %s, %s, %s)"
-        request_insert = (id_ruangan, nama_barang_dlm_ruangan, desc_barang_dlm_ruangan, qnty_barang_dlm_ruangan, uploaded_file.filename)
+        insert_query = "INSERT INTO barang_dlm_container (id_container, nama_barang_dlm_container, desc_barang_dlm_container, qnty_barang_dlm_container, gambar_barang_dlm_container) VALUES (%s, %s, %s, %s, %s)"
+        request_insert = (id_container, nama_barang_dlm_container, desc_barang_dlm_container, qnty_barang_dlm_container, uploaded_file.filename)
         cursor.execute(insert_query, request_insert)
         connection.commit()  # Commit changes to the database
         cursor.close()
         new_id = cursor.lastrowid  # Get the newly inserted book's ID\
         if new_id:
-            return jsonify({"title": nama_barang_dlm_ruangan, "message": "Inserted", "id_barang_dlm_ruangan": new_id}), 201
+            return jsonify({"title": nama_barang_dlm_container, "message": "Inserted", "id_barang_dlm_container": new_id}), 201
         return jsonify({"message": "Cant Insert Data"}), 500
 
-@barang_dlm_ruangan_endpoints.route('/delete/<id_barang_dlm_ruangan>', methods=['DELETE'])
-def delete(id_barang_dlm_ruangan):
+@barang_dlm_container_endpoints.route('/delete/<id_barang_dlm_container>', methods=['DELETE'])
+def delete(id_barang_dlm_container):
     connection = get_connection()
     cursor = connection.cursor()
-    delete_query = "DELETE FROM barang_dlm_ruangan WHERE id_barang_dlm_ruangan=%s"
-    delete_id = (id_barang_dlm_ruangan,)
+    delete_query = "DELETE FROM barang_dlm_container WHERE id_barang_dlm_container=%s"
+    delete_id = (id_barang_dlm_container,)
     cursor.execute(delete_query, delete_id)
     connection.commit()
     cursor.close()
@@ -72,14 +72,14 @@ def delete(id_barang_dlm_ruangan):
     }), 200
 
 
-@barang_dlm_ruangan_endpoints.route('/update/<id_barang_dlm_ruangan>', methods=['PUT'])
-def update(id_barang_dlm_ruangan):
+@barang_dlm_container_endpoints.route('/update/<id_barang_dlm_container>', methods=['PUT'])
+def update(id_barang_dlm_container):
     """Routes for module update a book"""
-    nama_barang_dlm_ruangan = request.form['nama_barang_dlm_ruangan']
-    desc_barang_dlm_ruangan = request.form['desc_barang_dlm_ruangan']
-    qnty_barang_dlm_ruangan = request.form['qnty_barang_dlm_ruangan']
+    nama_barang_dlm_container = request.form['nama_barang_dlm_container']
+    desc_barang_dlm_container = request.form['desc_barang_dlm_container']
+    qnty_barang_dlm_container = request.form['qnty_barang_dlm_container']
 
-    uploaded_file = request.files['gambar_barang_dlm_ruangan']
+    uploaded_file = request.files['gambar_barang_dlm_container']
     if uploaded_file.filename != '':
         file_path = os.path.join(UPLOAD_FOLDER, uploaded_file.filename)
         uploaded_file.save(file_path)
@@ -87,12 +87,12 @@ def update(id_barang_dlm_ruangan):
         connection = get_connection()
         cursor = connection.cursor()
 
-        update_query = "UPDATE barang_dlm_ruangan SET nama_barang_dlm_ruangan=%s, gambar_barang_dlm_ruangan=%s WHERE id_barang_dlm_ruangan=%s"
-        update_request = (nama_barang_dlm_ruangan, uploaded_file.filename, id_barang_dlm_ruangan)
+        update_query = "UPDATE barang_dlm_container SET nama_barang_dlm_container=%s, gambar_barang_dlm_container=%s WHERE id_barang_dlm_container=%s"
+        update_request = (nama_barang_dlm_container, uploaded_file.filename, id_barang_dlm_container)
         cursor.execute(update_query, update_request)
         connection.commit()
         cursor.close()
-        data = {"message": "updated", "id_container": id_barang_dlm_ruangan}
+        data = {"message": "updated", "id_container": id_barang_dlm_container}
         return jsonify(data), 200
 
 # @books_endpoints.route("/upload", methods=["POST"])
--- app.py
+++ app.py
@@ -10,6 +10,7 @@ from api.data_protected.endpoints import protected_endpoints
 from api.ruangan.endpoints import ruangan_endpoints
 from api.container.endpoints import container_endpoints
 from api.barang_dlm_ruangan.endpoints import barang_dlm_ruangan_endpoints
+from api.barang_dlm_container.endpoints import barang_dlm_container_endpoints
 from config import Config
 from static.static_file_server import static_file_server
 
@@ -31,6 +32,7 @@ app.register_blueprint(books_endpoints, url_prefix='/api/v1/books')
 app.register_blueprint(static_file_server, url_prefix='/static/')
 app.register_blueprint(ruangan_endpoints, url_prefix='/api/v1/ruangan')
 app.register_blueprint(barang_dlm_ruangan_endpoints, url_prefix='/api/v1/barang_dlm_ruangan')
+app.register_blueprint(barang_dlm_container_endpoints, url_prefix='/api/v1/barang_dlm_container')
 app.register_blueprint(container_endpoints, url_prefix='/api/v1/container')
 
 
